0f93a2272bd9eb8f80aee5fdeb97b8132f7be4a4
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-09-30 21:31:44 +0300

    Initial Commit

---
+++ CryptoPricesApp/CryptoPricesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPricesAppApp.swift
+//  CryptoPricesApp
+//
+//  Created by Danil Peregorodiev on 30.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPricesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
